01aec2ede665454bd420fb8d88b2db9ab1ad558e
Thiago Ramos <thiagoramos.al@gmail.com>
2021-04-07 22:42:26 -0300

    Finished index page and add tailwind

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule CrowdPomodoro.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:timex, "~> 3.7.3"}
+      {:timex, "~> 3.7.3"},
+      {:distillery, "~> 2.1"}
     ]
   end
 

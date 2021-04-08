85ac0b877301315a9da13cb9e2b9597911dcc623
Thiago Ramos <thiagoramos.al@gmail.com>
2021-04-07 23:16:31 -0300

    Added ssl

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -13,7 +13,7 @@ database_url =
 
 config :crowd_pomodoro, CrowdPomodoro.Repo,
   adapter: Ecto.Adapters.Postgres,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 

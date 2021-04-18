875de5aaf1e60c5baeeb1b0dd352fc4cea66256a
Thiago Ramos <thiagoramos.al@gmail.com>
2021-04-18 12:55:51 -0300

    Changing tailwind config to use purgecss

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: false,
+  purge: [
+    '../lib/**/*.ex',
+    '../lib/**/*.leex',
+    '../lib/**/*.eex',
+    './js/**/*.js'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
